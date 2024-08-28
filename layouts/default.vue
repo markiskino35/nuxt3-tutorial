33ba2214bc0443c5b7035a283632e198573cfaad
Alief Mukhlis <markiskino35@gmail.com>
2024-08-28 17:18:35 +0800

    feat: tambah tailwind css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/">Nuxt 3 Tutorial</NuxtLink>
-        <ul>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink class="font-bold" to="/">Nuxt 3 Tutorial</NuxtLink>
+        <ul class="flex gap-4">
           <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/products">Products</NuxtLink></li>
+          <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
         </ul>
       </nav>
     </header>
 
     <!-- output the page content -->
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
   </div>
